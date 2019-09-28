e9a7d16daa799ca6d6725093181f52b3b16d27b3
Your Name <you@example.com>
2019-09-28 15:56:03 +0800

    msg f

---
+++ sched/sched_test.go
@@ -0,0 +1,27 @@
+package sched
+
+import (
+	"testing"
+	"time"
+)
+
+func TestSched(t *testing.T) {
+	ok := false
+	Schedule(func() { ok = true }, time.Now().Add(time.Second))
+	time.Sleep(time.Second * 2)
+	if !ok {
+		t.FailNow()
+	}
+}
+
+func TestSchedTimedoutAlready(t *testing.T) {
+	ok := false
+	key := Schedule(func() { ok = true }, time.Now().Add(-time.Second))
+	if ok || key != 0 {
+		t.FailNow()
+	}
+	key = Schedule(func() { ok = true }, time.Now())
+	if !ok || key != 0 {
+		t.FailNow()
+	}
+}
